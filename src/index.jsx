73596c3148c3fa51b994d1164c0e315d333a6ebd
Lauryan <lauryan-gervais@hotmail.com>
2021-02-25 17:52:14 -0500

    modification de l'url

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import Appli from './Appli';
 
-const baseName = (window.location.href.search(/github\.io/) !== -1) ? "/panier-achats-pvt-gr2" : "";
+const baseName = (window.location.href.search(/github\.io/) !== -1) ? "panier-achats" : "";
 
 ReactDOM.render(
   <React.StrictMode>
