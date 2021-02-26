5e0caf2c0590a6232ccb5b3e40bf0612cc3c2300
Lauryan <lauryan-gervais@hotmail.com>
2021-02-25 20:46:38 -0500

    Mise a jour cours 25 fevrier

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -4,15 +4,55 @@ import tabProduits from './data/produits.json';
 import fbAppli from 'firebase/app';
 import "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDYXixKoQkDQRBQM6g_v0lspnYwWsUV2OM",
   authDomain: "veille-technologique-173fb.firebaseapp.com",
+  databaseURL: "https://veille-technologique-173fb-default-rtdb.firebaseio.com",
   projectId: "veille-technologique-173fb",
   storageBucket: "veille-technologique-173fb.appspot.com",
   messagingSenderId: "884338243671",
   appId: "1:884338243671:web:a5b8adf2d90fea7ba98b12",
   measurementId: "G-19TYS3Z8K8"
 };
+
+// Guide Firestore (https://firebase.google.com/docs/firestore)
+// Étape 1 : initialiser le service Firebase et le produit Firestore
+if(!fbAppli.apps.length) {
+  fbAppli.initializeApp(firebaseConfig);
+}
+
+const bd = fbAppli.firestore();
+
+// Étape 2 : utiliser l'API firestore
+
+// Exemple A : écrire des documents dans la collection "produits"
+tabProduits.forEach(
+  prd =>  bd.collection("produits").add(prd).then(
+    refDoc => console.log("ID du document ajouté : ", refDoc.id)
+  )
+);
+
+// Exemple B : lire des données dans une "collection"
+bd.collection("produits").get().then(
+  reponse => {
+    reponse.forEach(
+      doc => console.log("Produit dont le prix > 13$ : ", doc.data())
+    )
+  }
+);
+
+// Exemple C : observer les données en temps réel
+let observerProduits = bd.collection("produits").onSnapshot(
+  vue => vue.docChanges().forEach(
+    changement => {
+      if(changement.type=="added") {
+        console.log(changement.doc.data());
+      }
+    }
+  )
+);
+
 
 export default function ListeProduits(props) {
   return (
